2e9192a3471fa06aa1fe005e30947c4bde2076f5
Koloskov_Maxim <queetman@mail.ru>
2018-03-26 13:34:50 +0300

    Ver 1.2 Сделана работа над ошибками.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,28 +1,34 @@
 import java.util.Arrays;
-import java.util.Comparator;
 
 /**
- * Array based storage for Resumes
+ *Скорректированный вариант в соответствии с замечаниями:
+ * 1) не оставляй в коде так называемых "якорей" - закомментированного кода
+ 2) зачем ты в конструкторе инициализируешь все ячейки storage, Resume с uuid = zzz?
+ 3) в clear() заполняй ячейки массива null
+ 4) для того что бы не подсчитывать каждый раз в методе size() длину массива, заведи дополнительное поле size
+ используя эту переменную, реализация многих методов частично изменится
+ 5) в методе get() возвращай сразу storage[i] или null. Первые три строчки лишние
+ 6) по заданию у нас в массиве не может быть "дырок". Это значит, что все элементы плотно прилегают друг к другу. При удалении учитывай это. Ни null ни zzz не должно быть между элементами
+ 7) порядок элементов в getAll() нам пока не нужен. Просто возвращай Arrays.copyOfRange()
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[100000];
-    Resume nullElement = new Resume();
 
-    public ArrayStorage() {
-        nullElement.uuid = "zzz";
-        Arrays.fill(storage, nullElement);
-    }
-
+    private int storageLength = 10000;
+    private Resume[] storage = new Resume[storageLength];
 
     void clear() {
-        nullElement.uuid = "zzz";
-        Arrays.fill(storage, nullElement);
+
+        if (size() != 0) {
+            for (int i = 0; i < storage.length; i++) {
+                storage[i] = null;
+            }
+        } else System.out.println("Storage is empty");
     }
 
     //если ячейка пуста - сохранеиие.
     void save(Resume r) {
         for (int i = 0; i < storage.length; i++) {
-            if (storage[i].equals(nullElement)) {
+            if (storage[i] == null) {
                 storage[i] = r;
                 break;
             }
@@ -32,76 +38,45 @@ public class ArrayStorage {
     Resume get(String uuid) {
 
         Resume resume = null;
-        Resume notExistResume = new Resume();
-        notExistResume.uuid = "Element does not exist";
 
-
-        for (int i = 0; i < storage.length; i++) {
+        for (int i = 0; i < size(); i++) {
             if (storage[i].uuid.equals(uuid)) {
                 resume = storage[i];
                 break;
 
-
-            } else resume = notExistResume;
-
+            } else resume = null;
         }
-
         return resume;
-
     }
 
     void delete(String uuid) {
+        int j = 0;
 
+//поиск номера элемента на удаление
         for (int i = 0; i < storage.length; i++) {
             if (storage[i].toString().equals(uuid)) {
-                storage[i] = nullElement;
+                j = i;
+                break;
             }
+        }
+        //Сдвиг элементов, чтобы избежжать дырок в массиве
+        for (int i = j; i < storageLength - 1; i++) {
+
+            storage[i] = storage[i + 1];
         }
 
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-  /*
-   Вар.1
-   Resume[] getAll() {
-
-        Resume[] notNullResume = new Resume[10000];
-        Arrays.fill(notNullResume,nullElement);
-        int j = 0;
-
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != nullElement) {
-                notNullResume[j] = storage[i];
-                j++;
-            }
-        }
-
-        return notNullResume;
-    }*/
-//вар 2 получше
     Resume[] getAll() {
 
-        Resume[] resumeWithutNull;
-
-        Arrays.sort(storage, new Comparator<Resume>() {
-            @Override
-            public int compare(Resume o1, Resume o2) {
-                return o1.uuid.compareTo(o2.uuid);
-            }
-        });
-        resumeWithutNull = Arrays.copyOfRange(storage, 0, size());
-
-        return resumeWithutNull;
+        return Arrays.copyOfRange(storage, 0, size());
     }
 
     int size() {
         int i = 0;
 
         for (Resume aStorage : storage) {
-            if (aStorage != nullElement) i++;
-
+            if (aStorage != null) i++;
         }
         return i;
     }

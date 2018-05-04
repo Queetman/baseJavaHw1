afc79e544fc671616378209a0dc161b986573552
Koloskov_Maxim <queetman@mail.ru>
2018-05-04 14:52:15 +0300

    4.2 ПОдкорректирована структура проекта

---
+++ src/ru/storage/ArrayStorage.java
@@ -0,0 +1,27 @@
+package ru.storage;
+
+import ru.model.Resume;
+
+public class ArrayStorage extends AbstractArrayStorage {
+
+    @Override
+    protected int getIndex(String uuid) {
+
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].getUuid())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    protected void saveElement(Resume resume, int index) {
+        storage[size] = resume;
+    }
+
+    @Override
+    protected void deleteElement(int index) {
+        storage[index] = storage[size - 1];
+    }
+}

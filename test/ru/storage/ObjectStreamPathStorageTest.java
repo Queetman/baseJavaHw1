d7687d582674846373f6bd88784532243fcda9ae
Koloskov_Maxim <queetman@mail.ru>
2018-08-14 22:26:43 +0300

    9.2. Realized ObjectStreamPathStorage with tests

---
+++ test/ru/storage/ObjectStreamPathStorageTest.java
@@ -0,0 +1,13 @@
+package ru.storage;
+
+public class ObjectStreamPathStorageTest   extends AbstractStorageTest {
+
+
+    public ObjectStreamPathStorageTest() {
+        super(new ObjectStreamStorage(STORAGE_DIR));
+    }
+
+}
+
+
+

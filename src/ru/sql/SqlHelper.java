391df41d42d85da63553dbe33aa6fa79ecff9816
Koloskov_Maxim <queetman@mail.ru>
2018-10-10 11:09:14 +0300

    14.0. After lecture

--- src/ru/sql/SqlHelper.java
+++ src/ru/sql/SqlHelper.java
@@ -1,5 +1,7 @@
 package ru.sql;
 
+import ru.exception.StorageException;
+
 import java.sql.*;
 
 public class SqlHelper {
@@ -10,16 +12,28 @@ public class SqlHelper {
         connectionFactory = () -> DriverManager.getConnection(dbUrl, dbUser, dbPassword);
     }
 
-    public void execute(String query) {
-        execute(query, statement -> statement.execute());
-    }
-
     public <T> T execute(String sql, SqlExecutor<T> executor) {
         try (Connection conn = connectionFactory.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             return executor.execute(ps);
         } catch (SQLException e) {
             throw ExceptionUtil.convertException(e);
+        }
+    }
+
+    public <T> T transactionalExecute(SqlTransaction<T> executor) {
+        try (Connection conn = connectionFactory.getConnection()) {
+            try {
+                conn.setAutoCommit(false);
+                T res = executor.execute(conn);
+                conn.commit();
+                return res;
+            } catch (SQLException e) {
+                conn.rollback();
+                throw ExceptionUtil.convertException(e);
+            }
+        } catch (SQLException e) {
+            throw new StorageException(e);
         }
     }
 }
